6a7ce42dccdddb4d57d06156f06b004fbe33bf9f
s1140314-rgb <s1140314@dmhs.kh.edu.tw>
2025-10-17 16:17:47 +0800

    Add files via upload

---
+++ hw2.cpp
@@ -0,0 +1,12 @@
+    #include <iostream>
+    #include <string>
+    
+    int main() {
+    int age = 10086;
+    double height = 185.9;
+    std::string name = "xavier";
+    bool is_pass = true;
+    std::cout << "name: " << name << " height: " << height << std::endl;
+    std::cout << "age: " << age << " 是否及格: " << is_pass << std::endl;
+    return 0;
+    }
