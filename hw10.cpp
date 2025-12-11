19ce9ab7e5a48f1281018cf868d1329ddf955a27
s1140314-rgb <s1140314@dmhs.kh.edu.tw>
2025-12-11 09:39:13 +0800

    Add files via upload

---
+++ hw10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int password = 6;
+    int enterKey;
+    std::cout<<"輸入密碼(1-10)";
+    cin >> enterKey;
+    while (password != enterKey) {
+        cout << "密碼錯誤,再次輸入密碼(1-10)";
+        cin >> enterKey;
+    }
+    cout << " 恭喜你輸入正確 ";
+    
+}
